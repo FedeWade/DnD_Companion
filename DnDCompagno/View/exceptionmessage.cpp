f2ef63de7f5e051b683babc0059dd368cf40e9b4
Federico Ballarin <62607753+FedeWade@users.noreply.github.com>
2022-04-23 14:09:58 +0200

    Delete DnDCompagno directory

--- DnDCompagno/View/exceptionmessage.cpp
+++
@@ -1,8 +0,0 @@
-#include "exceptionmessage.h"
-
-ExceptionMessage::ExceptionMessage(QWidget *parent, const QString &msg) : QMessageBox(parent)
-{
-    setText(msg);
-    setWindowIcon(QIcon(":/Resources/Warning.svg"));
-    setWindowTitle("Attenzione!");
-}
